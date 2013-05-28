a3d3196abd7af9019fd10f8006599b50e2e381b1
antonhaw <>
2013-05-28 12:30:40 +0200

    keine for-Schelifen für Gewinnchnce erhöhen gute Bewertungsfunktion

--- TicTacToe_4x3/src/ai/AIPlayerImpl.java
+++ TicTacToe_4x3/src/ai/AIPlayerImpl.java
@@ -199,74 +199,74 @@ public class AIPlayerImpl {
 				}
                                 
                    //horizontal gewinnenchance erhöhen
-		for(int line=0; line<3; line++){
-			for(int column=0; column<4;column++){
-				if(getField(line, column).isX()){
-					//System.out.println("Akt X-Feld: "+getField(line+1, column+1));
-					if(checkHorizontalX(line, column)){
-                                            
-                                     System.out.println("Horizontal erhöhen");
-						if(getField(line, column+1).isX()){
-						//	System.out.println("Result: zeile: "+(line)+" spalte: "+(column));
-							return (line+1)*10+(column);
-						}else{
-							//System.out.println("Result: zeile: "+(line+1)+" spalte: "+(column+2));
-							return (line+1)*10+(column+2);
-						}
-					}
-				}
-			}
-		}             
-                   //diagonall gewinnchance erhöhen (\)
-                     for(int line=0; line<3;line++){
-                         for(int column=0; column<4;column++){
-                             if(getField(line,column).isX()){
-                                 if(checkDiagonalX2(line, column)){
-                                     System.out.println("diagonall \\");
-                                     if(getField(line-1,column-1).isX()){
-                                         return (line+2)*10+column+2;
-                                     }else{
-                                         return line*10+column;
-                                     }
-                                 }
-                             }
-                         }
-                     } 
+//		for(int line=0; line<3; line++){
+//			for(int column=0; column<4;column++){
+//				if(getField(line, column).isX()){
+//					//System.out.println("Akt X-Feld: "+getField(line+1, column+1));
+//					if(checkHorizontalX(line, column)){
+//                                            
+//                                     System.out.println("Horizontal erhöhen");
+//						if(getField(line, column+1).isX()){
+//						//	System.out.println("Result: zeile: "+(line)+" spalte: "+(column));
+//							return (line+1)*10+(column);
+//						}else{
+//							//System.out.println("Result: zeile: "+(line+1)+" spalte: "+(column+2));
+//							return (line+1)*10+(column+2);
+//						}
+//					}
+//				}
+//			}
+//		}             
+//                   //diagonall gewinnchance erhöhen (\)
+//                     for(int line=0; line<3;line++){
+//                         for(int column=0; column<4;column++){
+//                             if(getField(line,column).isX()){
+//                                 if(checkDiagonalX2(line, column)){
+//                                     System.out.println("diagonall \\");
+//                                     if(getField(line-1,column-1).isX()){
+//                                         return (line+2)*10+column+2;
+//                                     }else{
+//                                         return line*10+column;
+//                                     }
+//                                 }
+//                             }
+//                         }
+//                     } 
 		
-                //diagonall gewinnchance erhöhen (/)
-                     for(int line=0; line<3;line++){
-                         for(int column=0; column<4;column++){
-                             if(getField(line,column).isX()){
-                                 if(checkDiagonalX(line, column)){
-                                     System.out.println("Diagonall //");
-                                     if(getField(line-1,column+1).isX()){
-                                         return (line+2)*10+column;
-                                     }else{
-                                         return line*10+column+2;
-                                     }
-                                 }
-                             }
-                         }
-                     }          
-                     
+//                //diagonall gewinnchance erhöhen (/)
+//                     for(int line=0; line<3;line++){
+//                         for(int column=0; column<4;column++){
+//                             if(getField(line,column).isX()){
+//                                 if(checkDiagonalX(line, column)){
+//                                     System.out.println("Diagonall //");
+//                                     if(getField(line-1,column+1).isX()){
+//                                         return (line+2)*10+column;
+//                                     }else{
+//                                         return line*10+column+2;
+//                                     }
+//                                 }
+//                             }
+//                         }
+//                     }          
+//                     
                    
                                 
 		
-		//vertiakal gewinnchance erhöhen
-		for(int line=0; line<3; line++){
-			for(int column=0; column<4;column++){
-				if(getField(line, column).isX()){
-					if(checkVertikalX(line, column)){
-                                     System.out.println("Vertikal erhöhen");
-						if(getField(line-1, column).isX()){
-							return (line+2)*10+column+1;
-						}else{
-							return (line)*10+column+1;
-						}
-					}
-				}
-			}
-		}
+//		//vertiakal gewinnchance erhöhen
+//		for(int line=0; line<3; line++){
+//			for(int column=0; column<4;column++){
+//				if(getField(line, column).isX()){
+//					if(checkVertikalX(line, column)){
+//                                     System.out.println("Vertikal erhöhen");
+//						if(getField(line-1, column).isX()){
+//							return (line+2)*10+column+1;
+//						}else{
+//							return (line)*10+column+1;
+//						}
+//					}
+//				}
+//			}
+//		}
 		//System.out.println("bestMove");
 		int[] bm = getBestMove();
 		return (bm[0]+1)*10+bm[1]+1;
@@ -401,33 +401,35 @@ public class AIPlayerImpl {
 			for(int column=0; column<4;column++){
 				if(getField(line,column).isFree()){
 					int temp_mark = 0;
-					if(isField(line,column-1) && getField(line,column-1).isFree()){temp_mark++;}
-					if(isField(line,column+1) && getField(line,column+1).isFree()){temp_mark++;}
-					if(isField(line-1,column-1) && getField(line-1,column-1).isFree()){temp_mark++;}
-					if(isField(line-1,column+1) && getField(line-1,column+1).isFree()){temp_mark++;}
-					if(isField(line+1,column-1) && getField(line+1,column-1).isFree()){temp_mark++;}
-					if(isField(line+1,column+1) && getField(line+1,column+1).isFree()){temp_mark++;}
-					if(isField(line-1,column) && getField(line-1,column).isFree()){temp_mark++;}
-					if(isField(line+1,column) && getField(line+1,column).isFree()){temp_mark++;}
+					if(isField(line,column-1) && getField(line,column-1).isFree()){System.out.println("\t Frei1"); temp_mark++;}
+					if(isField(line,column+1) && getField(line,column+1).isFree()){System.out.println("\t Frei2"); temp_mark++;}
+					if(isField(line-1,column-1) && getField(line-1,column-1).isFree()){System.out.println("\t Frei3"); temp_mark++;}
+					if(isField(line-1,column+1) && getField(line-1,column+1).isFree()){System.out.println("\t Frei4"); temp_mark++;}
+					if(isField(line+1,column-1) && getField(line+1,column-1).isFree()){System.out.println("\t Frei5"); temp_mark++;}
+					if(isField(line+1,column+1) && getField(line+1,column+1).isFree()){System.out.println("\t Frei6"); temp_mark++;}
+					if(isField(line-1,column) && getField(line-1,column).isFree()){System.out.println("\t Frei7"); temp_mark++;}
+					if(isField(line+1,column) && getField(line+1,column).isFree()){System.out.println("\t Frei8"); temp_mark++;}
 					
 					//Oben-Bonus
-					if(isField(line-1,column) && getField(line-1,column).isX()){temp_mark++;}
-					if(isField(line+1,column) && getField(line+1,column).isX()){temp_mark++;}
+					if(isField(line-1,column) && getField(line-1,column).isX()){System.out.println("\t Oben5");temp_mark++;}
+					if(isField(line+1,column) && getField(line+1,column).isX()){System.out.println("\t Oben5");temp_mark++;}
 					
 					//Unten-Bonus
-					if(isField(line,column-1) && getField(line,column-1).isX()){temp_mark++;}
-					if(isField(line,column+1) && getField(line,column+1).isX()){temp_mark++;}
+					if(isField(line,column-1) && getField(line,column-1).isX()){System.out.println("\t Unten1");temp_mark++;}
+					if(isField(line,column+1) && getField(line,column+1).isX()){System.out.println("\t Unten2");temp_mark++;}
 					
 					//Diagonal-Bonus
-					if(isField(line-1,column+1) && isField(line+1,column-1) && getField(line-1,column+1).isX()){temp_mark++;}
-					if(isField(line-1,column-1) && isField(line+1,column+1) && getField(line-1,column-1).isX()){temp_mark++;}
-					if(isField(line+1,column+1) && isField(line-1,column-1) && getField(line+1,column+1).isX()){temp_mark++;}
-					if(isField(line+1,column-1) && isField(line-1,column+1)&& getField(line+1,column-1).isX()){temp_mark++;}
+					if(isField(line-1,column+1) && isField(line+1,column-1) && getField(line-1,column+1).isX()){System.out.println("\t Diagonal1"); temp_mark++;}
+					if(isField(line-1,column-1) && isField(line+1,column+1) && getField(line-1,column-1).isX()){System.out.println("\t Diagonal2"); temp_mark++;}
+					if(isField(line+1,column+1) && isField(line-1,column-1) && getField(line+1,column+1).isX()){System.out.println("\t Diagonal3"); temp_mark++;}
+					if(isField(line+1,column-1) && isField(line-1,column+1)&& getField(line+1,column-1).isX()){System.out.println("\t Diagonal4"); temp_mark++;}
+                                        if(isField(line+1,column-1) && getField(line+1,column-1).isX()){ System.out.println("\t Diagonal5"); temp_mark++;}
+                                        if(isField(line+2,column-2) && isField(line+1,column-1) && getField(line+2,column-2).isFree() && getField(line+1,column-1).isX()) {System.out.println("\t Diagonal6"); temp_mark++;}
 					
 					//Mögliche Reihe -Bonus
-					if(isField(line-1,column) && isField(line+1,column) && (getField(line-1,column).isX() || getField(line+1,column).isX())){temp_mark++;}
+					if(isField(line-1,column) && isField(line+1,column) && (getField(line-1,column).isX() || getField(line+1,column).isX())){System.out.println("\t Reihe1"); temp_mark++;}
 					
-					//System.out.println(getField(line, column) + "Mark: "+temp_mark);
+					System.out.println(getField(line, column) + "Mark: "+temp_mark);
 					if(temp_mark>mark){
 						mark = temp_mark;
 						result[0] = getField(line, column).getY();
